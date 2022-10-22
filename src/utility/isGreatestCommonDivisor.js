91eccd052bc6157b9e9b3cd26d04f8be2647ead8
vadymtel <vadymtel@gmail.com>
2022-10-22 12:17:17 +0300

    fix code style

--- src/utility/isGreatestCommonDivisor.js
+++ src/utility/isGreatestCommonDivisor.js
@@ -3,11 +3,14 @@ const isGreatestCommonDivisor = (num1, num2) => {
     return num1;
   }
   const minNum = Math.min(num1, num2);
+  let greatestCommonDivisor = 0;
   for (let i = minNum; i > 0; i -= 1) {
     if (num1 % i === 0 && num2 % i === 0) {
-      return i;
+      greatestCommonDivisor = i;
+      break;
     }
   }
+  return greatestCommonDivisor;
 };
 
 export default isGreatestCommonDivisor;
